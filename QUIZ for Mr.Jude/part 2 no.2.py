e3f8a332ad89e6295e20e44e3ebe3425697929bb
calvinshalim <55437075+calvinshalim@users.noreply.github.com>
2019-10-23 20:16:24 +0700

    Add files via upload

--- QUIZ for Mr.Jude/part 2 no.2.py
+++ QUIZ for Mr.Jude/part 2 no.2.py
@@ -1,26 +1,26 @@
 #%% 2
 filename = "data.txt"
-
-def displaydata(filename):
-    staffs = []
-    for line in open(filename,'r'):
-        spl = line.rstrip().split("#")
-        staffs.append(staff(spl[0],spl[1],spl[2],spl[3]))
-    return staffs 
+import re
 
 class staff():
     __ID = 0
-    __Name = ""
+    Name = ""
     __Position = ""
     __Salary = 0
     def __init__(self,ID,Name,Position,Salary):
         self.__ID = ID
-        self.__Name = Name
+        self.Name = Name
         self.__Position = Position
         self.__Salary = Salary
         
     def __str__(self):
-        return self.__ID +"  " +self.__Name+"  "+self.__Position+"  "+self.__Salary
+        return f"staff(__ID={self.__ID},name={self.Name},Position={self.__Position},Salary={self.__Salary})"
+    
+    def __repr__(self):
+        return f"staff(__ID={self.__ID},name={self.Name},Position={self.__Position},Salary={self.__Salary})"
+    
+    def __eq__(self,strID):
+        return self.__ID == strID
     
     def getID(self):
         return self.__ID
@@ -34,33 +34,128 @@ class staff():
     def getSalary(self):
         return self.__Salary
     
-    def addstaff(self):
-        newid = int(input("Enter New ID:"))
-        if len(newid) <= 5:
-            print("ID must have 5 characters ONLY!")
-        if len(newid[0]) == "S":
-            print("ID must start with an S")
-        newname = str(input("Input Name:"))
-        if len(newname) >= 20:
-            print("Name Too Long")
-        newpos = str(input("Input Position:"))      
+salary_range = {
+        "Staff":{
+            "MIN":3500000,
+            "MAX":7000000
+            },
+        "Officer":{
+                "MIN":7000001,
+                "MAX":10000000
+                },
+        "Manager":{
+                "MIN":10000001,
+                "MAX":1E+130
+            }
+        }
+ 
+
+def opendata(filename):
+    staffs = []
+    for line in open(filename,'r'):
+        spl = line.rstrip().split("#")
+        staffs.append(staff(spl[0],spl[1],spl[2],spl[3]))
+    return staffs 
+
+def savedata(staffs,filename):
+    _temp = ""
+    for staff in staffs:
+        _temp += f"{staff.getID()}#{staff.Name}#{staff.getPosition()}#{staff.getSalary()}\n" 
+    with open(filename, "w") as fp:
+        fp.write(_temp)
+        
+def printStaffs(staffs):
+    data = [["ID","Name","Position","Salary"]]
+    data += [[staff.getID(), staff.Name, staff.getPosition(), staff.getSalary()]
+            for staff in staffs] 
+    col_width = max(len(word) for wor in data for word in row) + 2
+    for row in data:
+        print("|".join(word.ljust(col_width) for word in row))
 #%%            
     
-def menu():
-    print("1.New  Staff\n2.Delete Stadd\n3.View Summary Data\n4.Save & Exit")
-    choice = str(input("input Choice"))
+def menu(staffs):
+    printStaffs(staffs)
     while True:
-        if choice == "1":     
-            print("New staff\n")
-        return addstaff()
-        
-        elif choice == "2":
-            delstaff = str(input("Input ID: "))
-            if delstaff == staff:
-                staffs.remove(delstaff)
-            else:
-                print("ID not exist")
-                
-        elif choice == "3":
-            
+        print("1.New  Staff\n2.Delete Stadd\n3.View Summary Data\n4.Save & Exit")
+        try:
+        choice = str(input("input Choice"))
+        if choice == "1":  
+            while True:
+                __ID = input("input new ID[Sxxxx]:")
+        if not re.match(r"S\d{4}", _id):
+            print("The format have to be SXXXX!")
+            continue
+        name = input("Input name [0..20]: ")
+        if len(name) >= 20:
+            print("Your name must not exceed 20 characters!")
+            continue
+        position = input(
+            "Input desired position [staff|officer|manager]: ").upper()
+        if position not in positions:
+            print("Position not available!")
+            continue
+        try:
+            salary = int(input(
+                f"Input desired salary for {position} [{positions[position]['min']}..{positions[position]['max']}]: "))
+            if not (salary >= positions[position]["min"] and salary <= positions[position]["max"]):
+                print("Salary not in range!")
+                continue
+        except ValueError:
+            print("Salary have to be int!")
+            continue
+        staff = Staff(_id, name, position, salary)
+        staffs.append(staff)
+        print(f"Successfully added a new staff!\n{staff}")
+        break
+    elif choice == 2:
+        idDelete = input("Input id [SXXXX]")
+        if not re.match(r"S\d{4}", idDelete):
+            print("The format have to be SXXXX!")
+            continue
+        if idDelete not in staffs:
+            print("ID not found!")
+            continue
+        staffs.pop(staffs.index(idDelete))
+        print("ID Removed!")
+    elif choice == 3:
+        staffSal = []
+        officerSal = []
+        managerSal = []
+        for staff in staffs:
+            if staff.getPosition() == "STAFF":
+                staffSal.append(int(staff.getSalary()))
+            elif staff.getPosition() == "OFFICER":
+                officerSal.append(int(staff.getSalary()))
+            elif staff.getPosition() == "MANAGER":
+                managerSal.append(int(staff.getSalary()))
+        printOut = "1. Staff\n"
+        printOut += f"Minimum Salary: {positions['STAFF']['min']}\n"
+        printOut += f"Maximum Salary: {positions['STAFF']['max']}\n"
+        try:
+            printOut += f"Average Salary: {sum(staffSal)/len(staffSal)}\n\n"
+        except ZeroDivisionError:
+            printOut += "Average Salary: 0\n\n"
+        printOut += "2. Officer\n"
+        printOut += f"Minimum Salary: {positions['OFFICER']['min']}\n"
+        printOut += f"Maximum Salary: {positions['OFFICER']['max']}\n"
+        try:
+            printOut += f"Average Salary: {sum(officerSal)/len(officerSal)}\n\n"
+        except ZeroDivisionError:
+            printOut += "Average Salary: 0\n\n"
+        printOut += "3. Manager\n"
+        printOut += f"Minimum Salary: {positions['MANAGER']['min']}\n"
+        printOut += f"Maximum Salary: {positions['MANAGER']['max']}\n"
+        try:
+            printOut += f"Average Salary: {sum(managerSal)/len(managerSal)}\n\n"
+        except ZeroDivisionError:
+            printOut += "Average Salary: 0\n\n"
+        print(printOut)
+    elif choice == 4:
+        break
+   except ValueError:
+       continue
+   
+stfs = opendata("data.txt")
+menu(stfs)
+savedata(stfs,"data2.txt")
         
